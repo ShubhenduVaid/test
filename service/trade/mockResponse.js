15381ac20e9a0663061e7bc603df7b3d8f962180
Shubhendu Vaid <Shubhendu_Vaid@epam.com>
2019-04-02 22:46:16 +0200

    Added Comments

--- service/trade/mockResponse.js
+++ service/trade/mockResponse.js
@@ -9,6 +9,7 @@ let response = [
   { pair: 'EUR JPY', buy: 120.589, sell: 120.491 }
 ];
 
+// Calcualtes randomly to increment/decrement 
 function calculateSign() {
   let randomNumber = Math.round(Math.random() * 20);
   if (randomNumber % 2 === 0) {
@@ -18,10 +19,14 @@ function calculateSign() {
   }
 }
 
+// Calcualtes random value between 1 and 10 (inclusive)
 function calculateValue() {
   return Math.round((Math.random() * 9 + 1));
 }
 
+/**
+ * PUBLIC API
+ */
 function makeResponse() {
   for (let index = 0; index < response.length; index++) {
     const element = response[index];
@@ -33,7 +38,10 @@ function makeResponse() {
       element['buy'] = +element['buy'] - +element['buy'] / value;
       element['sell'] = +element['sell'] - +element['sell'] / value;
     }
+    // Sometimes due to rounding off to 5 decimals digits turn out to be 0
+    // so redeclaring them  
     element['buy'] = (+element['buy'] !== 0) ? element['buy'].toFixed(5) : buy;
+    // Checking buy to keep buy > sell
     element['sell'] = (+element['buy'] !== 0 && +element['sell'] !== 0) ? element['sell'].toFixed(5) : sell;
   }
   return response;
